145b681e3b3cb4513068bf57abf12b55ae696e6a
okura8765 <okuhiro.8765@gmail.com>
2020-09-10 15:51:42 +0900

    create layout

---
+++ src/pc/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import InputBox from './InputBox';
+
+export default function Header(props) {
+    const {title} = props;
+
+    return (
+        <header>
+            <h1>{title.toUpperCase()}</h1>
+        </header>
+    );
+}
--- src/pc/pc.js
+++ src/pc/pc.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import {BrowserRouter,Route,Link} from "react-router-dom";
 import {fetchGetList,fetchApply,fetchDelete,fetchGetNumList,fetchGetSummary} from "../apis/fetchData"
+import InputBox from './components/InputBox';
+import Footer from './components/Footer';
+import Header from './components/Header';
 // fetchGetList("2020-08-01","2020-08-07")
 // fetchApply({reservations:{id:10,name:"name1",in:"2020-08-24T10:00:00.000Z",out:"2020-08-24T19:30:00.000Z"}})
 // fetchDelete(0)
@@ -11,17 +14,23 @@ export default class PC extends Component {
 
   constructor(props) {
     super(props);
+    this.title = '入室管理APP';
   }
-  
+
   // HTMLをどう描画するか
   render() {
-    return (<div> 
-      <p>PC</p>
-      <Link to="/sp">
-        <button>
-          go to SP
-        </button>
-      </Link>
-    </div>);
+    return (
+      <div className="container PC">
+        <div className="Header">
+          <Header title={this.title} />
+        </div>
+        <div className="InputBox">
+          <InputBox/>
+        </div>
+        <div className="Footer">
+          <Footer/>
+        </div>
+      </div>
+    );
   }
 }
