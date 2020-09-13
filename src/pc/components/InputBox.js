25fa044f97146426ecf9124b3fc0fda157d0746c
okura8765 <okuhiro.8765@gmail.com>
2020-09-13 20:59:52 +0900

    modify reservation in json shape by using immutability-helper

--- src/pc/components/InputBox.js
+++ src/pc/components/InputBox.js
@@ -1,41 +1,42 @@
 import React from 'react';
 import {fetchApply} from "../../apis/fetchData"
+import update from 'immutability-helper'
 
 export default class InputBox extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            name: '',
-            date: '',
-            in_time: '',
-            out_time: '',
-            isSubmitted: '',
+            reservation: {
+                name: '',
+                date: '',
+                in_room: '',
+                out_room: ''
+            }
         };
-        this.handleInTimeChange = this.handleInTimeChange.bind(this);
-        this.handleOutTimeChange = this.handleOutTimeChange.bind(this);
+        this.handleInRoomChange = this.handleInRoomChange.bind(this);
+        this.handleOutRoomChange = this.handleOutRoomChange.bind(this);
         this.handleDateChange = this.handleDateChange.bind(this);
         this.handleNameChange = this.handleNameChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleInTimeChange(e) {
-        this.setState({ in_time: e.target.value});
+    handleInRoomChange(e) {
+        this.setState({ reservation: update(this.state.reservation, { in_room: { $set: e.target.value } }) });
     }
-    handleOutTimeChange(e) {
-        this.setState({ out_time: e.target.value });
+    handleOutRoomChange(e) {
+        this.setState({ reservation: update(this.state.reservation, { out_room: { $set: e.target.value } }) });
     }
     handleDateChange(e) {
-        this.setState({ date: e.target.value });
+        this.setState({ reservation: update(this.state.reservation, { date: { $set: e.target.value } }) });
     }
     handleNameChange(e) {
-        this.setState({ name: e.target.value });
+        this.setState({ reservation: update(this.state.reservation, { name: { $set: e.target.value } }) });
     }
 
     handleSubmit(e) {
-        let tmp = fetchApply(this.state);
-        this.setState({ isSubmitted: 'success' });
-        // TODO: successを画面に表示する
+        let result = fetchApply(this.state);
+        window.alert(result)
     }
 
 
@@ -44,24 +45,23 @@ export default class InputBox extends React.Component {
             <div>
                 <form className="" onSubmit={this.handleSubmit}>
                     <div className="row">
-                        <input type="text" className="form-control col-md-5 input-element" placeholder="名前" onChange={this.handleNameChange} />
-                    </div>
-                    <div className="row">
+                        <div className="input-element">
+                            <input type="text" className="form-control" placeholder="名前" onChange={this.handleNameChange} />
+                        </div>
                         <div className="input-element">
                             <input type="date" className="form-control" placeholder="日付" onChange={this.handleDateChange} />
                         </div>
                         <div className="input-element">
-                            <input type="time" className="form-control" placeholder="入室時刻" onChange={this.handleInTimeChange} />
+                            <input type="time" className="form-control" placeholder="入室時刻" onChange={this.handleInRoomChange} />
                         </div>
                         <div className="input-element">
-                            <input type="time" className="form-control" placeholder="退室時刻" onChange={this.handleOutTimeChange} />
+                            <input type="time" className="form-control" placeholder="退室時刻" onChange={this.handleOutRoomChange} />
                         </div>
                         <div className="input-element">
                             <button type="submit"className="form-control btn btn-primary">送信</button>
                         </div>
                     </div>
                 </form>
-                <div className="">{this.state.isSubmitted}</div>
             </div>
         );
     }
