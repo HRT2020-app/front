3b1bd50741d4e5093c772a9d68a4c94b5c057c93
takahiro20 <itnglaac@gmail.com>
2020-09-15 16:26:32 +0900

    ShowItemに個々のstateを持たせる

--- src/pc/components/ShowItem.js
+++ src/pc/components/ShowItem.js
@@ -1,26 +1,38 @@
 import React, { Component } from 'react';
-import {BrowserRouter,Route,Link} from "react-router-dom";
-import {fetchGetList,fetchApply,fetchDelete,fetchGetNumList,fetchGetSummary} from "../../apis/fetchData"
-import { render } from '@testing-library/react';
 
-export default function ShowItem(props){
+export default class ShowItem extends React.Component{
 
-    const {item}=props;
-
-    return(
-        <tbody>
-            <tr>  
-                <th scope="row">{item.name}</th>
-                <td class="align-middle">{item.mon}</td>
-                <td class="align-middle">{item.tue}</td>
-                <td class="align-middle">{item.wed}</td>
-                <td class="align-middle">{item.thu}</td>
-                <td class="align-middle">{item.fri}</td>
-                <td class="align-middle">
-                    <button type="submit" class="btn btn-danger">削除</button>
-                </td>
-            </tr>
-        </tbody>
-    ); 
+    constructor(props){
+        super();
+        const item=props.item
+        this.state={
+            id: item.id,
+            name: item.name,
+            mon: item.mon,
+            tue: item.tue,
+            wed: item.wed,
+            thu: item.thu,
+            fri: item.fri,
+        }
+    }
+    
+    render(){
+        return(
+            <tbody>
+                <tr>  
+                    <th scope="row">{this.state.name}</th>
+                    <td class="align-middle">{this.state.mon}</td>
+                    <td class="align-middle">{this.state.tue}</td>
+                    <td class="align-middle">{this.state.wed}</td>
+                    <td class="align-middle">{this.state.thu}</td>
+                    <td class="align-middle">{this.state.fri}</td>
+                    <td class="align-middle">
+                        <button type="submit" class="btn btn-danger">削除</button>
+                    </td>
+                </tr>
+            </tbody>
+        ); 
+    }
 
 }
+
