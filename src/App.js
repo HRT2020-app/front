f36a789b509361bf817fcc46e658a071727ee158
SHIGAYUYA <shiga-yuya-yr@ynu.jp>
2020-10-23 22:13:24 +0900

    pc sp 自動切換え

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {BrowserRouter,Route} from "react-router-dom";
 import PC from './pc/pc';
 import SP from './sp/sp';
 
-const App = () => (
-  <BrowserRouter>
-    {/* BrowserRouter直下に置けるコンポーネントは1つだけ */}
-    <div>
-      {/* RouteはBrowserRouter以下ならばどこの階層に置いてもよい */}
-      <Route exact path="/" component={PC} /> {/*（1）*/}
-      <Route path="/sp" component={SP} />
-    </div>
-  </BrowserRouter>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        device: 'pc',
+    };
+  }
+
+  componentDidMount() {
+    // pc or sp
+    if (window.navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+      this.setState({ device: 'sp' });
+    } else {
+      this.setState({ device: 'pc' });
+    }
+  }
+
+  render() {
+    let showComponent;
+    if(this.state.device ===　"pc"){
+      showComponent = <PC/> 
+    }else if (this.state.device ===　"sp"){
+      showComponent = <SP/>
+    }
+
+    return (
+      <div>{showComponent}</div>
+    );
+  }
+}
 
 export default App;
