8058f36a2b4dbf01d1a8bd96a693890e0d774e52
SHIGAYUYA <shiga-yuya-yr@ynu.jp>
2020-10-24 13:45:12 +0900

    Updates

---
+++ precache-manifest.a2cacee8e1f13b51b762c23ba47cbe91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0469c464c1769e86293ca6dd27c7226d",
+    "url": "/front/index.html"
+  },
+  {
+    "revision": "317d93a0092e75e1effd",
+    "url": "/front/static/css/2.83880ee4.chunk.css"
+  },
+  {
+    "revision": "7e9730b167e6d3792d79",
+    "url": "/front/static/css/main.9a858e88.chunk.css"
+  },
+  {
+    "revision": "317d93a0092e75e1effd",
+    "url": "/front/static/js/2.a59d9cf3.chunk.js"
+  },
+  {
+    "revision": "b6ecc2ae71044227721a6052aea6820a",
+    "url": "/front/static/js/2.a59d9cf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e9730b167e6d3792d79",
+    "url": "/front/static/js/main.69ce871c.chunk.js"
+  },
+  {
+    "revision": "5bc7c753db0b12a72760",
+    "url": "/front/static/js/runtime-main.53746e49.js"
+  }
+]);
